992dedd956158dff97af0c30232439ba6d0a9729
DarkoMikhailovich <sergey51979@mail.ru>
2018-09-07 13:55:39 +0300

    1.3.JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+* <b><h3>Calculate - калькулятор</h3></b>
+* @author <i>Darko Mikhailovich</i> (sergey51979@mail.ru)
+*/
 public class Calculate{
+/**
+* <b>Вывод строки на консоль.</b>
+* Main
+* @param args - args.
+*/
 	public static void main(String[] args){
 			System.out.println("Hello World");
 	}
