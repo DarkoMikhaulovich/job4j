f552696f391fb44e527348e871be05a554342f90
DarkoMikhailovich <sergey51979@mail.ru>
2018-10-04 12:32:06 +0300

    3.2. Конвертер валюты. [#41583]

--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -22,14 +22,14 @@ public class ConverterTest {
     @Test
     public void when1DollarToRubleThen60() {
         Converter converter = new Converter();
-        int result = converter.DollarToRuble(1);
+        int result = converter.dollarToRuble(1);
         assertThat(result, is(60));
     }
 
     @Test
     public void when1EuroToRubleThen70() {
         Converter converter = new Converter();
-        int result = converter.EuroToRuble(1);
+        int result = converter.euroToRuble(1);
         assertThat(result, is(70));
     }
 }
