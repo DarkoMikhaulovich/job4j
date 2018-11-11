8f23486075c53feadb93c8db3d0bf2f256d29937
DarkoMikhailovich <sergey51979@mail.ru>
2018-11-12 01:10:44 +0300

    4.4. Максимум из трех чисел [#190]

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -1,7 +1,6 @@
-package ru.job4j.мах;
+package ru.job4j.max;
 
 import org.junit.Test;
-import ru.job4j.max.Max;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
